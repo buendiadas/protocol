cc2d926f71d3589c416b4438bdfcce2e2fbf1340
retotrinkler <rt@melonport.com>
2017-06-07 19:02:42 +0200

    ethpm.json setup

--- truffle.js
+++ truffle.js
@@ -1,8 +1,16 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+/* TODO
+ *  Insert here your 12 words mnemonic key
+ */
+const mnemonic = '..';
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
+      gas: 4709176,
       network_id: "*" // Match any network id
     },
     ropsten: {
@@ -13,9 +21,18 @@ module.exports = {
     kovan: {
       host: 'localhost',
       port: 8545,
-      gas: 4900000,
+      gasLimit: 409176,
+      gas: 4709176,
       network_id: 42,
       from: '0x00E0B33cDb3AF8B55CD8467d6d13BC0Ba8035acF'
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io"),
+      network_id: 4
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"),
+      network_id: 3
     }
   }
 };
